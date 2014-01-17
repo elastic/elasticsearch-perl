d1555ba0e3565dfbd43963f704de3b4d751428ae
Clinton Gormley <clint@traveljury.com>
2014-01-17 19:04:37 +0100

    Moved perl/lib and perl/t to top level and added NetCurl to main lib

--- t/lib/es_sync.pl
+++ t/lib/es_sync.pl
@@ -10,13 +10,13 @@ my $trace
 
 my $version = $ENV{ES_VERSION} || '';
 my $api = $version =~ /^0.90/ ? '0_90::Direct' : 'Direct';
-
+my $cxn = $ENV{ES_CXN} || do "default_cxn.pl" || die $!;
 my $es;
 if ( $ENV{ES} ) {
     $es = Elasticsearch->new(
         nodes    => $ENV{ES},
         trace_to => $trace,
-        cxn      => $ENV{ES_CXN} || 'LWP',
+        cxn      => $cxn,
         client   => $api,
     );
     eval { $es->ping; } or do {
