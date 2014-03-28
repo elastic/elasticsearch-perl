46d2f55c5eff4d248dc4a3ce9dae56c41248f0fb
Clinton Gormley <clint@traveljury.com>
2014-03-28 15:55:29 +0100

    Reenable all tests

--- test/run_junit_tests.sh
+++ test/run_junit_tests.sh
@@ -15,39 +15,39 @@ function run {
 
 ###### RUN TESTS #######
 
-#export JUNIT_OUTPUT_FILE=sync_tests.xml
-#run "Sync tests"        prove --harness=TAP::Harness::JUnit -l t/*/*.t
-#
-#export JUNIT_OUTPUT_FILE=yaml_lwp.xml
-#run 'YAML: HTTPTiny'    ./test/run_yaml_tests.pl --junit
-#
-#export JUNIT_OUTPUT_FILE=yaml_netcurl.xml
-#run 'YAML: NetCurl'     ./test/run_yaml_tests.pl  --junit --cxn NetCurl
+export JUNIT_OUTPUT_FILE=sync_tests.xml
+run "Sync tests"        prove --harness=TAP::Harness::JUnit -l t/*/*.t
+
+export JUNIT_OUTPUT_FILE=yaml_lwp.xml
+run 'YAML: HTTPTiny'    ./test/run_yaml_tests.pl --junit
 
 export JUNIT_OUTPUT_FILE=yaml_netcurl.xml
-run 'YAML: Hijk'        ./test/run_yaml_tests.pl  --junit --cxn Hijk -v --trace elasticsearch/rest-api-spec/test/update/
+run 'YAML: NetCurl'     ./test/run_yaml_tests.pl  --junit --cxn NetCurl
 
-#export ES_BODY='POST'
-#
-#export JUNIT_OUTPUT_FILE=sync_post.xml
-#run 'Sync tests - POST body' prove --harness=TAP::Harness::JUnit -l t/*/*.t
-#
-#export JUNIT_OUTPUT_FILE=yaml_post.xml
-#run 'YAML::HTTPTiny - POST body' ./test/run_yaml_tests.pl --junit
-#
-#perl -e 'exit $ENV{ES_VERSION}=~/^0.90.(10|[0-9])$/ ? 1 : 0';
-#if [ $? -eq 1 ];
-#    then echo "SKIPPING body-as-source: $ES_VERSION"
-#    exit $ERROR
-#fi
-#
-#export ES_BODY='source'
-#
-#export JUNIT_OUTPUT_FILE=sync_source.xml
-#run 'Sync tests - source body' prove --harness=TAP::Harness::JUnit -l t/*/*.t
-#
-#export JUNIT_OUTPUT_FILE=yaml_post.xml
-#run 'YAML::HTTPTiny - source body' ./test/run_yaml_tests.pl --junit
+export JUNIT_OUTPUT_FILE=yaml_netcurl.xml
+run 'YAML: Hijk'        ./test/run_yaml_tests.pl  --junit --cxn Hijk -v --trace
+
+export ES_BODY='POST'
+
+export JUNIT_OUTPUT_FILE=sync_post.xml
+run 'Sync tests - POST body' prove --harness=TAP::Harness::JUnit -l t/*/*.t
+
+export JUNIT_OUTPUT_FILE=yaml_post.xml
+run 'YAML::HTTPTiny - POST body' ./test/run_yaml_tests.pl --junit
+
+perl -e 'exit $ENV{ES_VERSION}=~/^0.90.(10|[0-9])$/ ? 1 : 0';
+if [ $? -eq 1 ];
+    then echo "SKIPPING body-as-source: $ES_VERSION"
+    exit $ERROR
+fi
+
+export ES_BODY='source'
+
+export JUNIT_OUTPUT_FILE=sync_source.xml
+run 'Sync tests - source body' prove --harness=TAP::Harness::JUnit -l t/*/*.t
+
+export JUNIT_OUTPUT_FILE=yaml_post.xml
+run 'YAML::HTTPTiny - source body' ./test/run_yaml_tests.pl --junit
 
 #########################
 
