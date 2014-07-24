81d2f5e28bfaac965f980510c83614dbb85ecacd
Clinton Gormley <clint@traveljury.com>
2014-07-24 17:48:16 +0200

    Released version 1.14

--- lib/Search/Elasticsearch.pm
+++ lib/Search/Elasticsearch.pm
@@ -5,7 +5,7 @@ use Moo 1.003;
 use Search::Elasticsearch::Util qw(parse_params load_plugin);
 use namespace::clean;
 
-our $VERSION = '1.13';
+our $VERSION = '1.14';
 
 my %Default_Plugins = (
     client      => [ 'Search::Elasticsearch::Client',       'Direct' ],
