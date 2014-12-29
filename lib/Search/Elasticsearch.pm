45207ba96d6339d810c03831532d890f5cca7b4e
Clinton Gormley <clint@traveljury.com>
2014-12-29 19:29:14 +0100

    Released v1.17 of Search::Elasticsearch, ::Async, ::Cxn::NetCurl

--- lib/Search/Elasticsearch.pm
+++ lib/Search/Elasticsearch.pm
@@ -5,7 +5,7 @@ use Moo 1.003;
 use Search::Elasticsearch::Util qw(parse_params load_plugin);
 use namespace::clean;
 
-our $VERSION = '1.16';
+our $VERSION = '1.17';
 
 my %Default_Plugins = (
     client      => [ 'Search::Elasticsearch::Client',       'Direct' ],
