0c1e32c2b306787d04aa2e7230ab9c8fdfef67c4
Clinton Gormley <clint@traveljury.com>
2015-05-07 18:22:33 +0200

    Increased version to 1.20

--- lib/Search/Elasticsearch.pm
+++ lib/Search/Elasticsearch.pm
@@ -5,7 +5,7 @@ use Moo 1.003 ();
 use Search::Elasticsearch::Util qw(parse_params load_plugin);
 use namespace::clean;
 
-our $VERSION = '1.19';
+our $VERSION = '1.20';
 
 my %Default_Plugins = (
     client      => [ 'Search::Elasticsearch::Client',       '1_0::Direct' ],
--- lib/Search/Elasticsearch/Async.pm
+++ lib/Search/Elasticsearch/Async.pm
@@ -1,13 +1,13 @@
 package Search::Elasticsearch::Async;
 
-use Search::Elasticsearch 1.19;
+use Search::Elasticsearch 1.20;
 use Promises 0.93 ();
 use parent 'Search::Elasticsearch';
 
 use Search::Elasticsearch::Util qw(parse_params);
 use namespace::clean;
 
-our $VERSION = '1.19';
+our $VERSION = '1.20';
 
 #===================================
 sub new {
