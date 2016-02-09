54b62685284838ddc7b08527234df1fe1bedfea2
Thomas Klausner <domm@plix.at>
2016-02-09 10:38:36 +0100

    Docs: document all script-options (update, bulk update)

--- lib/Search/Elasticsearch/Bulk.pm
+++ lib/Search/Elasticsearch/Bulk.pm
@@ -423,16 +423,9 @@ In this case, all you have to do is to pass in a list of IDs.
         },
         { id            => 2,
           lang          => 'mvel',
-          script        => '_ctx.source.counter+=incr',
-          params        => { incr => 1},
+          script        => { script }
           upsert        => { upsert doc }
         },
-        {
-          id            => 3,
-          lang          => 'groovy',
-          script_file   => 'counter'  # config/scripts/counter.groovy
-          params        => { incr => 42},
-        }
         ...
     );
 
@@ -441,7 +434,7 @@ The C<update()> helper method allows you to add multiple C<update> actions.
 It accepts the same parameters as L<Search::Elasticsearch::Client::2_0::Direct/update()>.
 An update can either use a I<partial doc> which gets merged with an existing
 doc (example 1 above), or can use a C<script> to update an existing doc
-(example 2 above). To use a script stored in a file, use C<script_file> (example 3).
+(example 2 above). More information on C<script> can be found here: L<Search::Elasticsearch::Client::2_0::Direct/update()>.
 
 =head1 REINDEXING DOCUMENTS
 
