50cf4dbce587141cf10dfeb2d5e25f065e85e912
Enrico Zimuel <e.zimuel@gmail.com>
2019-09-20 16:44:48 +0200

    Using OSS version for Elasticsearch

--- travis/run_es_docker.sh
+++ travis/run_es_docker.sh
@@ -4,8 +4,8 @@ if [ -z $ES_VERSION ]; then
     exit 1;
 fi;
 
-docker pull docker.elastic.co/elasticsearch/elasticsearch:${ES_VERSION}
-docker network create esnet;
+docker pull docker.elastic.co/elasticsearch/elasticsearch-oss:${ES_VERSION}
+docker network create esnet-oss;
 docker run \
   --rm \
   --publish 9200:9200 \
@@ -13,8 +13,8 @@ docker run \
   --env "path.repo=/tmp" \
   --env "repositories.url.allowed_urls=http://snapshot.*" \
   --env "discovery.type=single-node" \
-  --network=esnet \
-  --name=elasticsearch \
+  --network=esnet-oss \
+  --name=elasticsearch-oss \
   --detach \
-  docker.elastic.co/elasticsearch/elasticsearch:${ES_VERSION}
-docker run --network esnet --rm appropriate/curl --max-time 120 --retry 120 --retry-delay 1 --retry-connrefused --show-error --silent http://elasticsearch:9200
+  docker.elastic.co/elasticsearch/elasticsearch-oss:${ES_VERSION}
+docker run --network esnet-oss --rm appropriate/curl --max-time 120 --retry 120 --retry-delay 1 --retry-connrefused --show-error --silent http://elasticsearch-oss:9200
