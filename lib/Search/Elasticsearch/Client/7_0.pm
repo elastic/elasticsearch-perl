28dc2288d4ee3049a5fd278748b52d1ac4865e30
Enrico Zimuel <e.zimuel@gmail.com>
2021-03-01 13:39:49 +0100

    Prep for release 7.11.1

--- lib/Search/Elasticsearch/Client/7_0.pm
+++ lib/Search/Elasticsearch/Client/7_0.pm
@@ -17,7 +17,7 @@
 
 package Search::Elasticsearch::Client::7_0;
 
-our $VERSION='7.110';
+our $VERSION='7.711001';
 use Search::Elasticsearch 7.00 ();
 
 1;
--- lib/Search/Elasticsearch/Client/7_0/Async.pm
+++ lib/Search/Elasticsearch/Client/7_0/Async.pm
@@ -17,7 +17,7 @@
 
 package Search::Elasticsearch::Client::7_0::Async;
 
-our $VERSION='7.110';
+our $VERSION='7.711001';
 use Search::Elasticsearch::Client::7_0 7.00 ();
 
 1;
