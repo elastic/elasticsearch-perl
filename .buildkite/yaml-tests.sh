5386fac78272edc2f3a3040fd74d1a9b1e9754e0
Enrico Zimuel <e.zimuel@gmail.com>
2024-01-08 15:36:04 +0100

    Added groups log for buildkite

--- .buildkite/yaml-tests.sh
+++ .buildkite/yaml-tests.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Checkout the YAML test from Elasticsearch tag
+echo "--- Checkout YAML test from Elasticsearch"
 util/checkout_yaml_test.pl
 
 # Run YAML tests
+echo "--- Run YAML tests"
 test/run_yaml_tests.pl
